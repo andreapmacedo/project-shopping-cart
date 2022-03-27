9908c72ff0bcb4d05a1811d28b66665b09369729
Andre <aa.andremacedo@gmail.com>
2022-03-27 09:45:53 -0300

    adiciona solucao para os requisitos 8 e 9

--- script.js
+++ script.js
@@ -6,33 +6,32 @@ totalPrice.innerText = 0;
 // totalPrice.innerText = 'Total R$ 0.00';
 
 function renderLoad() {
-  const father = document.querySelector('.cart');
-  const statusCarregamento = document.createElement('p');
-  father.appendChild(statusCarregamento);
-  statusCarregamento.className = 'loading';
-  statusCarregamento.innerText = 'carregando...';
+  const section = document.querySelector('.cart');
+  const loadingElement = document.createElement('p');
+  section.appendChild(loadingElement);
+  loadingElement.className = 'loading';
+  loadingElement.innerText = 'carregando...';
 }
 
 function removeRenderLoad() {
-  const statusCarregamento = document.querySelector('.loading');
-  statusCarregamento.remove();
+  const loadingElement = document.querySelector('.loading');
+  loadingElement.remove();
 }
 
 async function calculateTotal() {
   const list = document.querySelectorAll('.cart__items li');
   let sum = 0;
-  // let count = 0;
   list.forEach((element) => {
     // console.log(element.innerHTML.split(' ')[1]);
     sum += parseFloat(element.innerHTML.split('PRICE: $')[1]);
-    // count += 1;
   });
+  totalPrice.innerText = sum;
   // *consulta https://pt.stackoverflow.com/questions/181922/formatar-moeda-brasileira-em-javascript
   // sum = sum.toLocaleString('pt-br', { minimumFractionDigits: 2 });
   // totalPrice.innerText = sum.toFixed(2);
-  totalPrice.innerText = sum;
   // totalPrice.innerText = `Itens: ${ count } unds.
   //  Subtotal: R$ ${sum}`;
+  // totalPrice.innerText = `Subtotal: R$ ${sum}`;
 }
 
 // const calculateTotal = async () => {
@@ -51,12 +50,6 @@ async function calculateTotal() {
 //   });
 //   totalPrice.innerText = sum.toFixed(2);
 // };
-/*
-totalPrice.innerText = `Itens: ${count} unds.
-Subtotal: R$ ${sum.toFixed(2)}`;
-*/
-// sum = sum.toLocaleString('pt-br', { minimumFractionDigits: 2 });
-// *consulta https://pt.stackoverflow.com/questions/181922/formatar-moeda-brasileira-em-javascript
 
 function setEmputCart() {
   cartItems.innerHTML = '';
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,29 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('1. Teste se fetchProducts é uma função.', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  test('2. Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada.', async () => {
+    await fetchProducts( 'computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('3. Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint correto', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  test('4. Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+
+  test('5. Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error("mensagem esperada aqui") para comparar com o objeto retornado da API.', async () => {
+    const response = await fetchProducts();
+    console.log(response);
+    expect(response).toEqual(new Error('You must provide an url'));
+  })
 });
